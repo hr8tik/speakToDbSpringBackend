65d7dbbf84084e5539b93ecbffbb98921804a189
Hr8tik <hrithiksengar64@gmail.com>
2026-01-12 15:58:06 +0530

    cros

--- src/main/java/com/example/demoChat/config/WebConfig.java
+++ src/main/java/com/example/demoChat/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8081")
+                .allowedOrigins("http://localhost:8081","https://customer-order-hub-2d10139c.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
